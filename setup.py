555601abfcaabb0f5260a95bbf2ed1f005e36db5
yasseridris <yasser@deeplite.ai>
2021-04-06 12:14:00 -0400

    Update version to 1.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='deeplite-profiler',
-    version='1.0.0',
+    version='1.1.0',
     description='Profiler for deep learning models',
     long_description=long_description,
     author='Deeplite',
