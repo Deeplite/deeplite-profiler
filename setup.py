0162587a898c7cc6df3bbcf1817038468df09e07
yasseridris <yasser@deeplite.ai>
2021-06-14 15:10:03 -0400

    Bump version to 1.1.9

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='deeplite-profiler',
-    version='1.1.8',
+    version='1.1.9',
     description='Profiler for deep learning models',
     long_description=long_description,
     long_description_content_type="text/markdown",
