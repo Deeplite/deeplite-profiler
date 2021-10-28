72848f57a021d4e69891c521cf22bf5d99518fec
yasseridris <yasser@deeplite.ai>
2021-10-28 10:15:42 -0400

    Bump version to 1.1.12

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='deeplite-profiler',
-    version='1.1.11',
+    version='1.1.12',
     description='Profiler for deep learning models',
     long_description=long_description,
     long_description_content_type="text/markdown",
