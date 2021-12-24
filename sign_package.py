ac89541c7d5c311501bdc4e62c9a7c3eddf42b0e
yasseridris <yasser@deeplite.ai>
2021-12-24 17:11:58 -0500

    Added sign_package.py

---
+++ sign_package.py
@@ -0,0 +1,43 @@
+import hashlib
+import os
+from cryptography.fernet import Fernet
+import base64
+
+def generate_checksum(fname, block_size=4096):
+    hash = hashlib.md5()
+    with open(fname, "rb") as f:
+        block = f.read(block_size)
+        while block:
+            hash.update(block)
+            block = f.read(block_size)
+    return hash.hexdigest()
+
+
+deeplite_key = os.getenv("DEEPLITE_KEY", "1234568123456812345681234568")
+
+def generate_signature(package_root):
+   sums = []
+   build_path = None
+   for path, dirs, files in os.walk(package_root):
+      for filename in files:
+         if filename.endswith(".so"):
+            sums.append(generate_checksum(os.path.join(path,filename)))
+            if not build_path:
+               build_path = path
+   signature =  ",".join(sums)
+   f = Fernet(base64.urlsafe_b64encode(deeplite_key.encode("utf-8")))
+   signature = f.encrypt(signature.encode("utf-8"))
+
+   return signature.decode('utf-8'), build_path
+
+
+
+
+def write_signature(sig, build_path):
+   sig_file_name = os.path.join(build_path,"deeplite-profiler.sig")
+   sig_file = open(sig_file_name, "w")
+   n = sig_file.write(sig)
+   sig_file.close()   
+
+if __name__ == "__main__":
+   write_signature(*generate_signature("build"))
