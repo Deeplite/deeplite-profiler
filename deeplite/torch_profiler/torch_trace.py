4ccc4c072608f3304a3f1e7efc02a76388faac08
Alexander Hoffman <alexander.hoffman@deeplite.ai>
2023-01-16 16:04:45 -0500

    reorganize torch tracing files

--- deeplite/torch_profiler/torch_trace.py
+++ deeplite/torch_profiler/torch_trace.py
@@ -1,11 +1,12 @@
 import warnings
 import re
+from collections import deque
 
 import torch
 import torch.jit
+import torch.nn as nn
 
 from deeplite.profiler.ir import Graph, Node, Variable
-from .flatten import Flatten
 
 __all__ = ['trace']
 
@@ -116,3 +117,27 @@ def trace(model, args=(), kwargs=None):
         nodes=nodes,
     )
     return graph
+
+
+def flatten(inputs):
+    queue = deque([inputs])
+    outputs = []
+    while queue:
+        x = queue.popleft()
+        if isinstance(x, (list, tuple)):
+            queue.extend(x)
+        elif isinstance(x, dict):
+            queue.extend(x.values())
+        elif isinstance(x, torch.Tensor):
+            outputs.append(x)
+    return outputs
+
+
+class Flatten(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, *args, **kwargs):
+        outputs = self.model(*args, **kwargs)
+        return flatten(outputs)
