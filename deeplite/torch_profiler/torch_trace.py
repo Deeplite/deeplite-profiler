9d47e1e2da6ff3c1ea1f6541ab13ded55adb049b
Alexander Hoffman <alexander.hoffman@deeplite.ai>
2023-01-18 16:51:43 -0500

    use torch trace for scope names

--- deeplite/torch_profiler/torch_trace.py
+++ deeplite/torch_profiler/torch_trace.py
@@ -11,74 +11,23 @@ from deeplite.profiler.ir import Graph, Node, Variable
 __all__ = ['trace']
 
 
-class scope_name_workaround(object):
-    """
-    This class makes sure that torch module scope names are saved in the jit trace
-    """
-    def __init__(self):
-        self.backup = None
-
-    def __enter__(self):
-        def _tracing_name(self_, tracing_state):
-            if not tracing_state._traced_module_stack:
-                return None
-            module = tracing_state._traced_module_stack[-1]
-            for name, child in module.named_children():
-                if child is self_:
-                    return name
-            return None
-
-        def _slow_forward(self_, *input, **kwargs):
-            tracing_state = torch._C._get_tracing_state()
-            if not tracing_state or isinstance(self_.forward, torch._C.ScriptMethod):
-                return self_.forward(*input, **kwargs)
-            if not hasattr(tracing_state, '_traced_module_stack'):
-                tracing_state._traced_module_stack = []
-            name = _tracing_name(self_, tracing_state)
-            if name:
-                tracing_state.push_scope('%s[%s]' % (self_._get_name(), name))
-            else:
-                tracing_state.push_scope(self_._get_name())
-            tracing_state._traced_module_stack.append(self_)
-            try:
-                result = self_.forward(*input, **kwargs)
-            finally:
-                tracing_state.pop_scope()
-                tracing_state._traced_module_stack.pop()
-            return result
-
-        self.backup = torch.nn.Module._slow_forward
-        setattr(torch.nn.Module, '_slow_forward', _slow_forward)
-
-    def __exit__(self, type, value, tb):
-        setattr(torch.nn.Module, '_slow_forward', self.backup)
-
-
 def filter_torch_scope(node):
     """
     given torch graph scope, returns module name in format matching mod.named_modules()
     """
-    scope = node.scopeName() \
-                .replace('Flatten/', '', 1) \
-                .replace('Flatten', '', 1)
-    scope_list = re.findall("\[.*?\]", scope)
-    mod_name = ''
-    if len(scope_list) >= 2:
-        for tok in scope_list[1:]:  # pop model name from path
-            mod_name += tok.strip('[').strip(']') + '.'
-        mod_name = mod_name.strip('.')
-    else:
-        # return empty string for model level nodes (inputs, outputs)
-        mod_name = ''
+    raw = node.scopeName()
+    raw_split = raw.split('/')
+    raw_name = raw_split[-1]
+    mod_name = raw_name.replace('__module.model', '')
+    mod_name = mod_name.strip('.')
     return mod_name
 
 
 def trace(model, args=(), kwargs=None):
     assert kwargs is None, 'Keyword arguments are not supported for now. ' \
                            'Please use positional arguments instead!'
-
-    with warnings.catch_warnings(record=True), scope_name_workaround():
-        graph, _ = torch.jit._get_trace_graph(Flatten(model), args, kwargs)
+    script = torch.jit.trace(Flatten(model), args, kwargs)
+    graph = script.inlined_graph
 
     variables = dict()
     for x in graph.nodes():
@@ -106,7 +55,8 @@ def trace(model, args=(), kwargs=None):
             },
             inputs=[variables[v] for v in x.inputs() if v in variables],
             outputs=[variables[v] for v in x.outputs() if v in variables],
-            scope=scope)
+            scope=scope
+        )
         nodes.append(node)
 
     graph = Graph(
